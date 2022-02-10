c260ebcd2c41136cd6a7f7c4b8a2fcfffafb78b1
RafsanAmin <rafsanamin2020@gmail.com>
2022-02-10 14:43:46 +0600

    ok

---
+++ Google_KickStart/cat_dog.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const input = fs.readFileSync('i.txt', 'utf-8').trim().split('\n');
+
+let currentline = 0;
+function readline() {
+  return input[currentline++];
+}
+
+const solve = (A, D, C, M, L) => {
+  let sol = false;
+  let CAT = C;
+  let DOG = D;
+  let DOGS = 0;
+  let CATS = 0;
+  for (let i = 0; i < A; i++) {
+    const animal = L[i];
+    if (animal === 'C') {
+      CATS++;
+    } else if (animal === 'D') {
+      DOGS++;
+    }
+  }
+  for (let i = 0; i < A; i++) {
+    const animal = L[i];
+    if (animal === 'C') {
+      if (CAT <= 0 && DOGS) {
+        sol = true;
+        break;
+      }
+      CAT--;
+      CATS--;
+    } else if (animal === 'D') {
+      if (DOG <= 0 && DOGS) {
+        sol = true;
+        break;
+      }
+      DOG--;
+      DOGS--;
+      CAT += M;
+    }
+  }
+  return sol ? 'NO' : 'YES';
+};
+
+let T = readline();
+for (let i = 1; i <= T; i++) {
+  let [A, D, C, M] = readline()
+    .split(' ')
+    .map((s) => Number(s));
+
+  let L = readline();
+  console.log(`Case #${i}: ${solve(A, D, C, M, L)}`);
+}
